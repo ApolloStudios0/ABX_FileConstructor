7ad8bd40d8b80df165298e9bffa81c4ee4c8834e
dev_nebula <Joelw@NEBULA>
2022-03-03 23:39:19 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace FileBuilder
+{
+    partial class FileConstructor
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.EncryptButton = new System.Windows.Forms.Button();
+            this.DecryptButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.KeyBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(1, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FileConstructor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(5, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(237, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "To Be Used For Educational Purposes Only";
+            // 
+            // EncryptButton
+            // 
+            this.EncryptButton.Location = new System.Drawing.Point(5, 68);
+            this.EncryptButton.Name = "EncryptButton";
+            this.EncryptButton.Size = new System.Drawing.Size(144, 138);
+            this.EncryptButton.TabIndex = 2;
+            this.EncryptButton.Text = "Encrypt";
+            this.EncryptButton.UseVisualStyleBackColor = true;
+            this.EncryptButton.Click += new System.EventHandler(this.ButtonClicker);
+            // 
+            // DecryptButton
+            // 
+            this.DecryptButton.Location = new System.Drawing.Point(155, 68);
+            this.DecryptButton.Name = "DecryptButton";
+            this.DecryptButton.Size = new System.Drawing.Size(144, 138);
+            this.DecryptButton.TabIndex = 3;
+            this.DecryptButton.Text = "Decrypt";
+            this.DecryptButton.UseVisualStyleBackColor = true;
+            this.DecryptButton.Click += new System.EventHandler(this.ButtonClicker);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 219);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Decryption Key";
+            // 
+            // KeyBox
+            // 
+            this.KeyBox.Location = new System.Drawing.Point(12, 237);
+            this.KeyBox.Multiline = true;
+            this.KeyBox.Name = "KeyBox";
+            this.KeyBox.Size = new System.Drawing.Size(287, 31);
+            this.KeyBox.TabIndex = 5;
+            // 
+            // FileConstructor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 280);
+            this.Controls.Add(this.KeyBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.DecryptButton);
+            this.Controls.Add(this.EncryptButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FileConstructor";
+            this.Text = "FileConstructor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button EncryptButton;
+        private System.Windows.Forms.Button DecryptButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox KeyBox;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileBuilder
+{
+    public partial class FileConstructor : Form
+    {
+        /* 
+         * Hello, this project is a child of Abraxas (An Open Source Remote Administration Tool) [Currently Private]
+         * Although this algorithm is used all over the world, this specific project must be used for educational purposes only.
+         * I, the creator, am not responsible for any actions, and or damages, caused by this software.
+         * You bear the full responsibility of your actions and acknowledge that this software was created for educational purposes only.
+         * This software's main purpose is NOT to be used maliciously, or on any system that you do not own, or have the right to use.
+         * By using this software, you automatically agree to the above.
+         * 
+         * Sail safe.
+         */
+
+        public FileConstructor()
+        {
+            InitializeComponent();
+            FormBorderStyle = FormBorderStyle.FixedSingle; // No Resize
+        }
+
+        private void ButtonClicker(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+
+            // Obtain Name of Button & Apply Function
+            if (KeyBox.Text != "")
+            {
+                try
+                {
+                    switch (button.Name)
+                    {
+                        // Encrypts The File & Creates New Version 'FileName.Extension.encrypted'
+                        case "EncryptButton":
+                            Functions.FileEncrypt(Functions.ObtainFileName(), KeyBox.Text);
+                            MessageBox.Show("Encrypted File With Extension '.encrypted' :  It has been placed in the same directory.");
+                            break;
+
+                        // Decrypts The File & Creates Overwrites Encrypted File
+                        case "DecryptButton":
+                            string FileChosen = Functions.ObtainFileName();
+                            Functions.FileDecrypt(FileChosen, FileChosen + ".decrypted", KeyBox.Text);
+                            MessageBox.Show("Decrypted File With Extension '.decrypted' :  It has been placed in the same directory.");
+                            break;
+                    }
+                }
+                catch {}
+            }
+            else { MessageBox.Show("Type In A Key For Decryption", "Missing Key", MessageBoxButtons.OK); }
+        }
+    }
+}
